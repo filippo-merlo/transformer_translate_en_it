b354194f99fafc74d69916ea47684b71b1f293c2
Filippo <filippo99merlo@gmail.com>
2024-08-08 17:17:58 +0200

    Refactor config.py to update cache directory and model path

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-CACHE_DIR = '/mnt/cimec-storage6/users/filippo.merl/transformer_it_en/cache'
-MODEL_PATH = '/mnt/cimec-storage6/users/filippo.merl/transformer_it_en/cache/models'
+CACHE_DIR = '/mnt/cimec-storage6/users/filippo.merlo/transformer_it_en/cache'
+MODEL_PATH = '/mnt/cimec-storage6/users/filippo.merlo/transformer_it_en/cache/models'
