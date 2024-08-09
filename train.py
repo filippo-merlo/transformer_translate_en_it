d59a904c9ff2a928a0390d111d2ee1a359a10df9
Filippo <filippo99merlo@gmail.com>
2024-08-09 13:15:57 +0200

    Refactor train.py to set device to "cuda" for GPU usage

--- train.py
+++ train.py
@@ -7,6 +7,7 @@ import torch.optim as optim
 from dataset import *
 from config import *
 
+'''
 # Check if a GPU is available and use it
 if torch.cuda.is_available():
     device = torch.device("cuda")
@@ -19,6 +20,8 @@ elif torch.backends.mps.is_available() and torch.backends.mps.is_built():
 else:
     device = torch.device("cpu")
     print("Using CPU")
+'''
+device = "cuda"
 
 import torch
 
