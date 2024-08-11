7ca28a2c9457e7f8d0fcdf40d9b0ae8128d83428
Filippo <filippo99merlo@gmail.com>
2024-08-11 15:01:19 +0200

    chore: Add print statement to debug attention mask shape

--- train.py
+++ train.py
@@ -156,6 +156,8 @@ def create_masks(eng_batch, it_batch, tokenizer=None):
     decoder_self_attention_mask = (look_ahead_mask + decoder_padding_mask.unsqueeze(1).unsqueeze(2).float()) * NEG_INFTY
     decoder_cross_attention_mask = encoder_padding_mask.unsqueeze(1).unsqueeze(2).float() * NEG_INFTY
     
+    print(encoder_self_attention_mask.shape)
+    print(decoder_self_attention_mask.shape)
     # Return the computed attention masks for the encoder and decoder
     return encoder_self_attention_mask, decoder_self_attention_mask, decoder_cross_attention_mask
 
