f7865baafb43d33475733ca97dcb6dd404a39273
Filippo <filippo99merlo@gmail.com>
2024-08-11 23:14:36 +0200

    Refactor dataset.py to use custom tokenizer and update vocabulary

--- dataset.py
+++ dataset.py
@@ -103,7 +103,7 @@ elif TOKENIZATION_LEVEL == 'word':
         spaced_token_list = list(itertools.chain(*list(itertools.chain(*ll))))
         return spaced_token_list
 
-    TOKENIZER = word_tokenize
+    TOKENIZER = custom_tokenizer
     italian_vocabulary = [START_TOKEN, ' ']
     english_vocabulary = [START_TOKEN, ' ']
 
