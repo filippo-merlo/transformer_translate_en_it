88e78f79c3f31e4af2a5df21c86ae41bc4466c28
Filippo <filippo99merlo@gmail.com>
2024-08-17 12:48:10 +0200

    ki

--- train.py
+++ train.py
@@ -152,12 +152,11 @@ for epoch in range(num_epochs):
 
         #train_losses.append(loss.item())
         if batch_num % 100 == 0:
-            print(labels)
-            print(it_predictions)
             print(f"Iteration {batch_num} : {loss.item()}")
             print(f"English: {eng_batch[0]}")
             print(f"Italian Translation: {it_batch[0]}")
             it_sentence_predicted = torch.argmax(it_predictions[0], axis=1)
+            print(it_sentence_predicted)
             predicted_sentence = ""
             if TOKENIZATION_LEVEL == 'word_piece':
                 ids_sentence = []
